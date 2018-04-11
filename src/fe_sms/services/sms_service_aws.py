2d3d035abbc2708d0237b3c855317f5c0b023b7c
Fernando Espíndola <fernando.espindola@hp.com>
2018-04-11 17:58:30 -0300

    arrumando a view

--- src/fe_sms/services/sms_service_aws.py
+++ src/fe_sms/services/sms_service_aws.py
@@ -1,16 +1,10 @@
 import boto3
 
-from fe_sms.models import AWSMensagem
-
 
 class SMSService(object):
     def __init__(self):
         session = boto3.Session()
         self.sns = session.client('sns')
-
-    @staticmethod
-    def create_message(usuario, telefone):
-        return AWSMensagem.objects.create(usuario=usuario, entidade=usuario.entity, telefone=telefone)
 
     def send_message(self, phone, message):
         response = self.sns.publish(PhoneNumber=phone, Message=message)
--- src/fe_sms/views.py
+++ src/fe_sms/views.py
@@ -2,21 +2,23 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from fe_sms.models import AWSMensagem
 from fe_sms.serializers import TelefoneModelSerializer
-from fe_sms.services.sms_service_aws import SMSService
 
 
 class EnviarMensagemAPIView(APIView):
 
     def post(self, request, format=None):
+        usuario = request.user,
+        entidade = request.user.entity,
         serializer = TelefoneModelSerializer(data=request.data)
         if serializer.is_valid():
-            user = self.request.user
-            entity = user.entity
-            telefone = serializer.save(usuario=user, entidade=entity)
-            message = SMSService.create_message(user,  telefone)
-            data = {
-                'uuid': str(message.uuid)
-            }
+            telefone = serializer.save(usuario=usuario, entidade=entidade)
+            message = AWSMensagem.objects.create(
+                usuario=request.user,
+                entidade=request.user.entity,
+                telefone=telefone
+            )
+            data = {'uuid': str(message.uuid)}
             return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
