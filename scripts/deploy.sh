fa88d7f262bc1b303cb3ad05fdcbe994a51bb4ed
Fernando Espíndola <fernando.espindola@hp.com>
2018-04-12 16:01:08 -0300

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,10 @@
 echo `pwd`
 
 ./scripts/az login --service-principal --username $AZURE_APP_ID --password $AZURE_PASSWORD --tenant $AZURE_TENANT_ID
-./scripts/az aks install-cli
 ./scripts/az aks get-credentials --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_CLUSTER_NAME
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
 
 /usr/local/bin/kubectl apply -f ./k8s/
