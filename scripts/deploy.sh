45c6cef978a7eeec7e4d8cff29da36ba1fa4597e
Fernando Espíndola <fernando.espindola@hp.com>
2018-04-12 15:52:40 -0300

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo `pwd`
+
+./scripts/az login --service-principal --username $AZURE_APP_ID --password $AZURE_PASSWORD --tenant $AZURE_TENANT_ID
+./scripts/az aks install-cli
+./scripts/az aks get-credentials --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_CLUSTER_NAME
+
 /usr/local/bin/kubectl apply -f ./k8s/
